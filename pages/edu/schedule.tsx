a0e04cb2227757bfe03b73a866adf54476e4201d
leshen <lexuar2000@gmail.com>
2022-07-27 09:38:58 +0300

    Test page for showing schedule

--- pages/edu/schedule.tsx
+++ pages/edu/schedule.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IPair } from '../../app/models/IPair';
+import {
+	useByTabnumMutation,
+	usePersonalQuery,
+} from '../../app/services/edu/ScheduleService';
 
 const Schedule = () => {
-	return (
-		<div>
-			Hello
+	const { data: schedule, isLoading } = usePersonalQuery();
+	const [getByTabnum, { isLoading: byTabnumLoading }] = useByTabnumMutation();
+	const [s, setS] = useState<IPair[] | null>(null);
+
+	const getgetget = async () => {
+		const res = await getByTabnum(21378);
+		if ('data' in res) {
+			setS(res.data);
+		}
+	};
+
+	const getJSXPair = (pair: IPair) => (
+		<div
+			key={
+				pair.date +
+				pair.number +
+				pair.time +
+				pair.subject_name +
+				pair.subgroup_id
+			}
+		>
+			{JSON.stringify(pair)}
 		</div>
+	);
+
+	return (
+		<>
+			<div>
+				{!isLoading &&
+					schedule?.map((pair) => (
+						<div
+							key={
+								pair.date +
+								pair.number +
+								pair.time +
+								pair.subject_name +
+								pair.subgroup_id
+							}
+						>
+							JSON.stringify(pair)
+						</div>
+					))}
+			</div>
+			<button onClick={getgetget}> GETGETGET </button>
+			<div>
+				<div>{byTabnumLoading && 'ЗАГРУЗКА'}</div>
+				<div>{s && !byTabnumLoading ? JSON.stringify(s) : ''}</div>
+			</div>
+		</>
 	);
 };
 
