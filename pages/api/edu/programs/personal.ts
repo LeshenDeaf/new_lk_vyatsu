6ba2a5e86cf67f463e56d2d7231680f641fd9c72
leshen <lexuar2000@gmail.com>
2022-08-26 13:47:10 +0300

    Created api for getting personal study plan

---
+++ pages/api/edu/programs/personal.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import vyatsuApi from '../../../../app/services/VyatsuApi';
+
+export default async function handle(
+	req: NextApiRequest,
+	res: NextApiResponse<any>
+) {
+	return new Promise<void>((resolve) => {
+		if (req.method === 'GET') {
+			vyatsuApi
+				.get('/api_mobile/v2/edu/programs/by_user/', {
+					headers: { Authorization: req.headers.authorization || '' },
+				})
+				.then((r: AxiosResponse<any>) => {
+					res.status(200).json(r.data);
+					return resolve();
+				})
+				.catch((e) => {
+					res.status(e.response.status).json(e.response.data);
+					return resolve();
+				});
+		}
+	});
+}
