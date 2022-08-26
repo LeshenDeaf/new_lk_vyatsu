3ba5ab41dc81076008920bc3ef3c26511f3b602e
leshen <lexuar2000@gmail.com>
2022-08-26 15:35:05 +0300

    Added getting data from server

--- pages/edu/programs.tsx
+++ pages/edu/programs.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import Program from "../../app/components/ui/programs/Program";
+import axios from 'axios';
+import React from 'react';
+import Program from '../../app/components/ui/programs/Program';
+import { ProgramsApiResponse } from '../../app/models/api/edu/programsTypes';
+import { wrapper } from '../../app/store/store';
 
-const Programs = () => {
-  return <Program />;
-}
+const Programs = ({programs}: {programs: ProgramsApiResponse}) => {
+	return <Program programs={programs} />;
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+	(store) => async () => {
+		const { auth } = store.getState();
+		const programs = await axios.get(
+			process.env.APP_URL + '/api/edu/programs/personal',
+			{
+				headers: {
+					authorization: `Bearer ${auth.token}`,
+				},
+			}
+		);
+
+		return {
+			props: {
+				programs: programs.data as ProgramsApiResponse,
+			},
+		};
+	}
+);
 
 export default Programs;
