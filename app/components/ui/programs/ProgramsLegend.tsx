a72b7f2b7761a83a8a1c057edb17793697e9bc31
leshen <lexuar2000@gmail.com>
2022-08-29 13:49:37 +0300

    Refactoring

--- app/components/ui/programs/ProgramsLegend.tsx
+++ app/components/ui/programs/ProgramsLegend.tsx
@@ -1,33 +1,36 @@
 import React, { memo } from 'react';
+import { ProgramColors } from '../../../models/api/edu/programsTypes';
 import BirdLogo from '../../shared/BirdLogo';
 
-export default memo(function ProgramsLegend() {
+interface Props {
+	colors: ProgramColors;
+}
+
+export const TypeColor = {
+	universal: '#C0E2EF',
+	common: '#FFF8B6',
+	prof: '#C4EAAB',
+	other: '#E9BBFF',
+};
+
+export default memo(function ProgramsLegend({ colors }: Props) {
 	return (
 		<>
 			<div className="font-[500] text-[24px] mb-[15px]">
 				Текущий учебный план
 			</div>
 			<div className="flex flex-col">
-				<div className="flex items-center my-[10px]">
-					<BirdLogo color="#C0E2EF" className="mr-[20px]" />
-
-					<div>Дисциплины формирующие общепрофессинальные компетенции</div>
-				</div>
-				<div className="flex items-center my-[10px]">
-					<BirdLogo color="#FFF8B6" className="mr-[20px]" />
-
-					<div>Дисциплины формирующие общие (универсальные) компетенции</div>
-				</div>
-				<div className="flex items-center my-[10px]">
-					<BirdLogo color="#E9BBFF" className="mr-[20px]" />
-
-					<div>Дисциплины формирующие факультативные компетенции</div>
-				</div>
-				<div className="flex items-center my-[10px]">
-					<BirdLogo color="#C4EAAB" className="mr-[20px]" />
-
-					<div>Дисциплины формирующие профессинальные компетенции</div>
-				</div>
+				{colors.used.map((colorType: string) => (
+					<div key={colorType} className="flex items-center my-[10px]">
+						<BirdLogo
+						//@ts-ignore
+							color={TypeColor[colorType] || ''}
+							className="mr-[20px]"
+						/>
+						{/*@ts-ignore*/}
+						<div>{colors.descriptions[colorType]}</div>
+					</div>
+				))}
 			</div>
 			<div className="font-[400] mt-[10px]">
 				<span className="font-[600]">
