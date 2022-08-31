d8380533c63bca35e6244fa915c1c3a098f6fa80
leshen <lexuar2000@gmail.com>
2022-08-31 15:32:32 +0300

    Made reauthentication

--- app/services/BasicVyatsu.ts
+++ app/services/BasicVyatsu.ts
@@ -1,4 +1,14 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { useDispatch } from 'react-redux';
+import {
+	BaseQueryFn,
+	createApi,
+	FetchArgs,
+	fetchBaseQuery,
+	FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+import { AuthResponse } from '../models/api/auth/types';
+import { setAuthData } from '../store/reducers/AuthSlice';
+import { unsetUserData } from '../store/reducers/UserSlice';
 import { RootState } from '../store/store';
 
 export const baseQuery = fetchBaseQuery({
@@ -6,7 +16,6 @@ export const baseQuery = fetchBaseQuery({
 	prepareHeaders: (headers, { getState }) => {
 		const token = (getState() as RootState).auth.token;
 
-		// If we have a token set in state, let's assume that we should be passing it.
 		if (token) {
 			headers.set('Authorization', `Bearer ${token}`);
 		}
@@ -16,7 +25,37 @@ export const baseQuery = fetchBaseQuery({
 	mode: 'cors',
 });
 
+const baseQueryWithReauth: BaseQueryFn<
+	string | FetchArgs,
+	unknown,
+	FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+	let result = await baseQuery(args, api, extraOptions);
+
+	if (result.error && result.error.status === 401) {
+		console.log('tried to refresh')
+		const refreshResult = await baseQuery(
+			'/api/auth/refresh/',
+			api,
+			extraOptions
+		);
+
+		console.log(refreshResult);
+
+		if (refreshResult.data) {
+			// store the new token
+			api.dispatch(setAuthData({token: refreshResult.data.token, isAuth: true}));
+			// retry the initial query
+			result = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(unsetUserData());
+		}
+	}
+
+	return result;
+};
+
 export const basicVyatsu = createApi({
-	baseQuery,
+	baseQuery: baseQueryWithReauth,
 	endpoints: () => ({}),
 });
