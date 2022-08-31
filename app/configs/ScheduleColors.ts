3d9593fb53c83272f2fa1d13e5677849ce9a52d5
leshen <lexuar2000@gmail.com>
2022-08-31 14:47:56 +0300

    Different colors for odd and even days. Refactoring

---
+++ app/configs/ScheduleColors.ts
@@ -0,0 +1,4 @@
+export const ScheduleColors = {
+  odd: '#6BCB2C',
+  even: '#4080F5',
+};
--- pages/edu/schedule.tsx
+++ pages/edu/schedule.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { IDaySchedule } from '../../app/models/schedule';
 import {
 	useByTabnumMutation,
@@ -8,42 +8,45 @@ import dayjs from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
 import DaySchedule from '../../app/components/ui/schedule/DaySchedule';
 import Link from 'next/link';
+import { ScheduleColors } from '../../app/configs/ScheduleColors';
 
 const Schedule = () => {
 	const { data: schedule, isLoading } = usePersonalQuery();
 	const [getByTabnum, { isLoading: byTabnumLoading }] = useByTabnumMutation();
+	// const {
+	// 	data: teacherSchedule,
+	// 	isLoading: isLoadingT,
+	// } = useTeacherQuery();
 	const [s, setS] = useState<IDaySchedule[] | null>(null);
 
 	dayjs.extend(require('dayjs/plugin/customParseFormat'));
 	dayjs.locale(require('dayjs/locale/ru'));
 
-	const getgetget = async () => {
-		const res = await getByTabnum(21378);
+	const teacherClicked = useCallback(async (tabnum: number) => {
+		const res = await getByTabnum(tabnum);
 		if ('data' in res) {
+			console.log(res.data)
 			setS(res.data);
 		}
-	};
+	}, [getByTabnum]);
 
-	const getJSXDaySchedule = (schedule: IDaySchedule) => (
-		<DaySchedule schedule={schedule} dayjs={dayjs} key={uuidv4()} />
+	const getJSXDaySchedule = (schedule: IDaySchedule, index: number) => (
+		<DaySchedule
+			color={index % 2 === 0 ? ScheduleColors.odd : ScheduleColors.even}
+			schedule={schedule}
+			dayjs={dayjs}
+			teacherClicked={teacherClicked}
+			key={`${schedule.date}-${schedule.day_of_week}`}
+		/>
 	);
 
 	return (
 		<>
 			<div>{!isLoading && schedule?.map(getJSXDaySchedule)}</div>
-			<button onClick={getgetget}> GETGETGET </button>
 			<div>
 				<div>{byTabnumLoading && 'ЗАГРУЗКА'}</div>
 				<div>{s && !byTabnumLoading ? s.map(getJSXDaySchedule) : ''}</div>
 			</div>
-			<Link href='/'>
-				<a
-					draggable="false"
-					className=""
-				>
-					TEST
-				</a>
-			</Link>
 		</>
 	);
 };
