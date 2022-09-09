d766e1fc07d48f5bd9da2b79ac503a9d0d007dd3
leshen <lexuar2000@gmail.com>
2022-09-09 15:47:15 +0300

    Added header, close and moved styles to external file

--- app/components/ui/modal/Modal.tsx
+++ app/components/ui/modal/Modal.tsx
@@ -1,15 +1,24 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { animated, config, Transition } from 'react-spring';
 import { useOnClickOutside } from 'usehooks-ts';
+import Image from 'next/image';
+import classes from './Modal.module.scss';
 
 interface Props {
 	isVisible: boolean;
 	setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
 	isLoading?: boolean;
+	header?: string;
 	children: React.ReactNode;
 }
 
-export default function Modal({ isVisible, setIsVisible, isLoading, children }: Props) {
+export default function Modal({
+	isVisible,
+	setIsVisible,
+	isLoading,
+	header,
+	children,
+}: Props) {
 	const ref = useRef<HTMLDivElement | null>(null);
 	// const [isVisible, setIsVisible] = useState(false);
 
@@ -25,35 +34,57 @@ export default function Modal({ isVisible, setIsVisible, isLoading, children }: 
 		)
 	);
 
-  useEffect(() => {
-    if(isVisible){
-      document.body.style.overflow = 'hidden';
-      ref.current?.scrollTo(0, 0);
-    } else {
-      document.body.style.overflow = 'scroll';
-    }  
-  }, [isVisible])
+	useEffect(() => {
+		if (isVisible) {
+			document.body.style.overflow = 'hidden';
+			ref.current?.scrollTo(0, 0);
+		} else {
+			document.body.style.overflow = 'scroll';
+		}
+	}, [isVisible]);
 
 	return (
 		<Transition
 			items={isVisible}
-			from={{ opacity: 0,}}
-			enter={{ opacity: 1,}}
-			leave={{ opacity: 0,}}
+			from={{ opacity: 0 }}
+			enter={{ opacity: 1 }}
+			leave={{ opacity: 0 }}
 			reverse={isVisible}
 			config={config.stiff}
 		>
 			{(styles, item) =>
-				item && <animated.div
-				className={`fixed top-0 left-0 z-50 w-[100vw] h-[100vh] bg-[rgba(20,20,20,.4)]`}
-				style={styles}
-			>
-				<div ref={ref} className='w-1/2 mx-auto mt-10 p-10 bg-white rounded-lg overflow-scroll h-4/5'>
-					{isLoading ? 'LOADING' : children}
-				</div>
-			</animated.div>
+				item && (
+					<animated.div className={classes.wrapper} style={styles}>
+						<div ref={ref} className={classes.modal}>
+							<div className={classes.header}>
+								<p>{header}</p>
+								<div
+									className="flex justify-center align-middle h-[28px] w-[28px] p-1 rounded-full hover:bg-[rgba(255,255,255,.1)] cursor-pointer"
+									onClick={() => setIsVisible(false)}
+								>
+										<Image
+											src="/images/close.svg"
+											alt="close"
+											draggable="false"
+											width="18"
+											height="18"
+											className="m-auto"
+											objectFit="contain"
+											layout="intrinsic"
+										/>
+									{/* <img
+										className="m-auto h-[18px] w-[18px]"
+										src="/images/close.svg"
+									/> */}
+								</div>
+							</div>
+							<div className={classes.body}>
+								{isLoading ? 'LOADING' : children}
+							</div>
+						</div>
+					</animated.div>
+				)
 			}
 		</Transition>
-		
 	);
 }
