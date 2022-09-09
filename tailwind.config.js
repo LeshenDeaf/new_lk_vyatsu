856bf6d03ea48e0b157e36a0676c4f273d78d38f
leshen <lexuar2000@gmail.com>
2022-09-09 08:59:22 +0300

    added new styles and deleted extra ones

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,20 @@ module.exports = {
         'vyatsu-blue-shadow': 'rgba(46, 158, 203, 0.2)',
         'vyatsu-shadow': 'rgba(34, 47, 162, 0.2)',
         'vyatsu-shadow-light': 'rgba(34, 47, 162, 0.1)',
-        'vyatsu-review-button-shadow': 'rgba(27, 96, 255, 0.25)',
         'vyatsu-programs-universal': '#C0E2EF',
         'vyatsu-programs-common': '#FFF8B6',
         'vyatsu-programs-prof': '#C4EAAB',
         'vyatsu-programs-other': '#E9BBFF',
       },
+      dropShadow: {
+        'blue': '0 2px 30px rgba(34,47,162,0.2)',
+        'button': '0px 12px 20px rgba(27, 96, 255, 0.25)'
+      },
+      boxShadow: {
+        'side-blue': '0px 20px 20px rgba(46, 158, 203, 0.2)',
+        'l-blue': '0px 0px 40px rgba(34, 47, 162, 0.1)',
+      },
+      
     },
     fontFamily: {
       'sans': ['Roboto'],
