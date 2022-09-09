5fdd858e3da7b406887c77099f8f3f84ad5a7b84
leshen <lexuar2000@gmail.com>
2022-09-09 15:47:47 +0300

    Renamed component, moved styles to external file

--- app/components/ui/schedule/DaySchedule.tsx
+++ app/components/ui/schedule/DaySchedule.tsx
@@ -3,36 +3,37 @@ import React, { FC } from 'react';
 import capitalizeFirstLetter from '../../../helpers/capitalize-first-letter';
 import { IDaySchedule } from '../../../models/schedule';
 import { v4 as uuidv4 } from 'uuid';
-import Pair from './PairGroup';
+import PairGroup from './PairGroup';
+import styles from './Schedule.module.scss';
 
 interface Props {
 	color: string;
 	schedule: IDaySchedule;
 	dayjs: any;
-	teacherClicked: (tabnum: number) => void;
+	teacherClicked: (tabnum: number, fio: string) => void;
 }
 
 const DaySchedule: FC<Props> = ({ color, schedule, dayjs, teacherClicked }) => {
 	const myFormat = 'DD.MM.YY';
 
 	return (
-		<div className="min-h-[205px] sm:min-h-[130px] bg-[#F7F8FA] mb-4 last:mb-0 flex flex-col sm:flex-row sm:rounded-tl-2xl sm:rounded-bl-2xl">
+		<div className={styles.daySchedule}>
 			{/*  дата  */}
 			<div
-				className="sm:min-w-[103px] min-h-[70px] sm:min-h-[130px] flex justify-start sm:justify-start items-center sm:items-center sm:flex-col p-4 sm:py-5 sm:rounded-tl-2xl sm:rounded-bl-2xl"
+				className={styles.dateLabel}
 				style={{ backgroundColor: color }}
 			>
-				<div className="text-white font-bold text-4xl">
+				<div className={styles.date}>
 					{dayjs(schedule.date, myFormat).format('DD')}
 				</div>
-				<div className="text-white font-light ml-4 sm:ml-0">
+				<div className={styles.day}>
 					{capitalizeFirstLetter(dayjs(schedule.date, myFormat).format('MMMM'))}
 				</div>
 			</div>
 			{/*  пары  */}
 			<div>
 				{schedule.pairs.map((pairGroup) => (
-					<Pair pairGroup={pairGroup} key={uuidv4()} teacherClicked={teacherClicked} />
+					<PairGroup pairGroup={pairGroup} key={uuidv4()} teacherClicked={teacherClicked} />
 				))}
 			</div>
 		</div>
--- app/components/ui/schedule/PairGroup.tsx
+++ app/components/ui/schedule/PairGroup.tsx
@@ -1,28 +1,36 @@
+import Link from 'next/link';
 import React, { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { IPairGroup } from '../../../models/schedule';
+import styles from './Schedule.module.scss';
 
 interface Props {
 	pairGroup: IPairGroup;
-	teacherClicked: (tabnum: number) => void;
+	teacherClicked: (tabnum: number, fio: string) => void;
 }
 
 const PairGroup: FC<Props> = ({ pairGroup, teacherClicked }) => {
 	return (
-		<div className="flex flex-col p-4 pr-8">
-			<div className="text-vyatsu-blue font-bold">
+		<div className={styles.pairGroup}>
+			<div className={styles.time}>
 				Пара {pairGroup.number}. С {pairGroup.time.replace('-', ' – ')}
 			</div>
 			{pairGroup.pairs.map((pair) => (
-				<div key={uuidv4()}>
+				<div className={styles.pair} key={uuidv4()}>
 					{pair.subject_name}, {pair.norm_comment},{' '}
 					<u
 						className="cursor-pointer"
-						onClick={() => teacherClicked(pair.sotr_tabnum)}
+						onClick={() => teacherClicked(pair.sotr_tabnum, pair.sotr_fio)}
 					>
 						{pair.sotr_fio}
 					</u>{' '}
-					{pair.mesto}
+					{pair.mesto.substring(0, 4) === 'http' ? (
+						<Link href={pair.mesto}>
+							<a className="underline" target="_blank">Ссылка на занятие</a>
+						</Link>
+					) : (
+						pair.mesto
+					)}
 				</div>
 			))}
 		</div>
