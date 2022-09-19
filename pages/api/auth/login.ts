776eee8524d464e27c9865a9f3d2ca8ac0651d65
leshen <lexuar2000@gmail.com>
2022-09-19 13:33:42 +0300

    bugfix

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -13,33 +13,29 @@ export default async function handle(
 	res: NextApiResponse<AuthResponse>
 ) {
 	return new Promise<void>((resolve) => {
-		if (req.method === 'POST') {
-			const reqData = {
-				login: req.body.login,
-				password: req.body.password,
-			} as LoginRequest;
+		const reqData = {
+			login: req.body.login,
+			password: req.body.password,
+		} as LoginRequest;
 
-			vyatsuApi
-				.post('/api_mobile/v2/auth/login/', reqData)
-				.then((result: AxiosResponse<AuthAPIResponse>) => {
-					setCookieTokens(
-						res,
-						result.data.access_token,
-						result.data.refresh_token
-					);
+		vyatsuApi
+			.post('/api_mobile/v2/auth/login/', reqData)
+			.then((result: AxiosResponse<AuthAPIResponse>) => {
+				setCookieTokens(
+					res,
+					result.data.access_token,
+					result.data.refresh_token
+				);
 
-					res.status(200).json({
-						token: result.data.access_token,
-						isAuth: true,
-					});
-					return resolve();
-				})
-				.catch((error) => {
-					res.status(error.response.status).json(error.response.data);
-					return resolve();
+				res.status(200).json({
+					token: result.data.access_token,
+					isAuth: true,
 				});
-		}
-
-		return resolve();
+				return resolve();
+			})
+			.catch((error) => {
+				res.status(error.response.status).json(error.response.data);
+				return resolve();
+			});
 	});
 }
