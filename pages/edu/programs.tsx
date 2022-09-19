93e45acb601e8cf32e9a0055e90fb574e2512fc9
leshen <lexuar2000@gmail.com>
2022-09-19 11:45:34 +0300

    Data is loaded on client to be able to refresh tokens

--- pages/edu/programs.tsx
+++ pages/edu/programs.tsx
@@ -1,34 +1,41 @@
-import axios from 'axios';
 import { NextPage } from 'next';
-import React from 'react';
 import Program from '../../app/components/ui/programs/Program';
-import { ProgramsApiResponse } from '../../app/models/api/edu/programsTypes';
+import { useProgramsQuery } from '../../app/services/edu/ProgramsService';
 import { setTitle } from '../../app/store/reducers/TitleSlice';
 import { wrapper } from '../../app/store/store';
 
-const Programs: NextPage<{ programs: ProgramsApiResponse }> = ({
-	programs,
-}) => {
-	return <Program programs={programs} />;
+const Programs: NextPage = () => {
+	const { data: programs, isLoading, isError } = useProgramsQuery();
+
+	if (isError) {
+		return <div>При загрузке данных произошла ошибка</div>;
+	}
+
+	return (
+		<>
+			{isLoading && 'Загрузка...'}
+			{!isLoading && programs && <Program programs={programs} />}
+		</>
+	);
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
 	(store) => async () => {
-		const { auth } = store.getState();
-		const programs = await axios.get(
-			process.env.APP_URL + '/api/edu/programs/personal',
-			{
-				headers: {
-					authorization: `Bearer ${auth.token}`,
-				},
-			}
-		);
+		// const { auth } = store.getState();
+		// const programs = await axios.get(
+		// 	process.env.APP_URL + '/api/edu/programs/personal',
+		// 	{
+		// 		headers: {
+		// 			authorization: `Bearer ${auth.token}`,
+		// 		},
+		// 	}
+		// );
 
 		store.dispatch(setTitle('Программы'));
 
 		return {
 			props: {
-				programs: programs.data as ProgramsApiResponse,
+				// programs: programs.data as ProgramsApiResponse,
 			},
 		};
 	}
