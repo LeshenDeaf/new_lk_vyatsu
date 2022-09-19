481ecc954b9ddc01d83e90cba0189601e6fb9120
leshen <lexuar2000@gmail.com>
2022-09-19 14:38:49 +0300

    Moved styles to the other folder

--- app/components/screens/loaders/PageLoader.tsx
+++ app/components/screens/loaders/PageLoader.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useEffect, useState } from 'react';
-import styles from './PageLoader.module.scss';
-import { Router, useRouter } from 'next/router';
+import { Router } from 'next/router';
+import { FC, useEffect, useState } from 'react';
+import styles from '../../../../styles/PageLoader.module.scss';
 
 interface PageLoaderProps {
 	children: JSX.Element;
@@ -18,13 +18,18 @@ const PageLoader: FC<PageLoaderProps> = ({ children }) => {
 			Router.events.off('routeChangeComplete', () => setLoading(false));
 			Router.events.off('routeChangeError', () => setLoading(false));
 		};
-	}, [Router.events]);
+	}, []);
 
 	return (
 		<>
 			{loading ? (
 				<div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 bg-opacity-10 opacity-100 flex flex-col items-center justify-center">
-					<div className={styles.loader + " ease-linear rounded-full border-4 border-t-4 border-gray-400 h-12 w-12 mb-4"}></div>
+					<div
+						className={
+							styles.loader +
+							' ease-linear rounded-full border-4 border-t-4 border-gray-400 h-12 w-12 mb-4'
+						}
+					></div>
 					<h2 className="text-center text-black text-xl font-semibold">
 						Loading...
 					</h2>
