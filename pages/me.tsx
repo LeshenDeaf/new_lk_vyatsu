09ffb3f00d68793ad008b16d2c7971ed0ead34e0
leshen <lexuar2000@gmail.com>
2022-09-19 14:40:34 +0300

    layout fix

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.scss';
-import Meta from '../app/utils/Meta';
 import { useAppSelector } from '../app/hooks/redux';
 import { selectUser } from '../app/store/reducers/UserSlice';
 
@@ -8,21 +6,11 @@ const Home: NextPage = () => {
 	const { data: user } = useAppSelector(selectUser);
 
 	return (
-		<div className={styles.container}>
-			<Meta title="Главная страница" description="Главная страница" />
+		<>
+			<div>{user && `${user.fio.full}`}</div>
+			<div>{JSON.stringify(user) || '!!!'}</div>
+		</>
+	);
+};
 
-			<main className={styles.main}>
-				<div>
-					<div>{ user && `${user.fio.full}` }</div>
-					<div>{ JSON.stringify(user) || '!!!'}</div>
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-
-			</footer>
-		</div>
-	)
-}
-
-export default Home
+export default Home;
