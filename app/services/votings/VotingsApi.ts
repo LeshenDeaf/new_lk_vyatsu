10559887f7625adb7dd012de730c1107d99e6936
leshen <lexuar2000@gmail.com>
2022-09-19 16:36:58 +0300

    Added voting and questions endpoints

--- app/services/votings/VotingsApi.ts
+++ app/services/votings/VotingsApi.ts
@@ -1,12 +1,23 @@
 import { IVoting } from '../../models/api/votings/types';
 import { basicVyatsu } from '../BasicVyatsu';
+import { IQuestion } from './../../models/api/votings/types';
 
 export const votingsApi = basicVyatsu.injectEndpoints({
 	endpoints: (builder) => ({
 		votings: builder.query<IVoting[], void>({
 			query: () => 'api/votings/list',
 		}),
+		voting: builder.query<IVoting, number>({
+			query: (votingId) => `api/votings/${votingId}`,
+		}),
+		questions: builder.query<IQuestion[], number>({
+			query: (vote_id: number) => ({
+				url: `api/votings/questions`,
+				method: 'POST',
+				body: {vote_id}
+			}),
+		})
 	}),
 });
 
-export const { useVotingsQuery } = votingsApi;
+export const { useVotingsQuery, useVotingQuery, useQuestionsQuery } = votingsApi;
