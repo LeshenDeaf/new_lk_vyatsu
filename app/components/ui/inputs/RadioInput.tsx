cc47d38a71b8d02cdb270e5b4baf0baac1adebfa
leshen <lexuar2000@gmail.com>
2022-09-20 15:28:30 +0300

    refactoring

--- app/components/ui/inputs/RadioInput.tsx
+++ app/components/ui/inputs/RadioInput.tsx
@@ -1,23 +1,21 @@
-import React, { FC } from 'react'
-import { IInputProps } from './types'
+import React, { FC } from 'react';
+import { IInputProps } from './types';
 
 const RadioInput: FC<IInputProps> = (props) => {
-  return (
-    <div>
-    <label>
-      <input
-        name={`${props.name}`}
-        required={props.isRequired}
-        value={props.defaultValue}
-        onChange={props.handleChange}
-        type="radio"
-      />
-      {props.label}
-    </label>
-
-    
-  </div>
-  )
-}
+	return (
+		<div>
+			<label>
+				<input
+					name={`${props.name}`}
+					required={props.isRequired}
+					value={props.defaultValue}
+					onChange={props.handleChange}
+					type="radio"
+				/>
+				{props.label}
+			</label>
+		</div>
+	);
+};
 
 export default RadioInput;
