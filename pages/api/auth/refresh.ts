75474ed6c9d2bb50f24b4925c14794b84d0aeb7a
leshen <lexuar2000@gmail.com>
2022-09-29 15:52:35 +0300

    refactoring

--- pages/api/auth/refresh.ts
+++ pages/api/auth/refresh.ts
@@ -14,30 +14,29 @@ export default async function handle(
 	res: NextApiResponse<AuthResponse>
 ) {
 	return new Promise<void>((resolve) => {
-		if (req.method === 'POST' || req.method === 'GET') {
-			const reqData = {
-				refresh_token: req.body.refresh_token || parseCookies({ req }).vyatsu_r_token || '',
-			} as RefreshRequest;
+		const reqData = {
+			refresh_token:
+				req.body.refresh_token ?? parseCookies({ req }).vyatsu_r_token ?? '',
+		} as RefreshRequest;
 
-			vyatsuApi
-				.post('/api_mobile/v2/auth/refresh/', reqData)
-				.then((result: AxiosResponse<AuthAPIResponse>) => {
-					setCookieTokens(
-						res,
-						result.data.access_token,
-						result.data.refresh_token
-					);
+		vyatsuApi
+			.post('/api_mobile/v2/auth/refresh/', reqData)
+			.then((result: AxiosResponse<AuthAPIResponse>) => {
+				setCookieTokens(
+					res,
+					result.data.access_token,
+					result.data.refresh_token
+				);
 
-					res.status(200).json({
-						token: result.data.access_token,
-						isAuth: true,
-					});
-					return resolve();
-				})
-				.catch((error) => {
-					res.status(error.response.status).json(error.response.data);
-					return resolve();
+				res.status(200).json({
+					token: result.data.access_token,
+					isAuth: true,
 				});
-		}
+				return resolve();
+			})
+			.catch((error) => {
+				res.status(error.response.status).json(error.response.data);
+				return resolve();
+			});
 	});
 }
