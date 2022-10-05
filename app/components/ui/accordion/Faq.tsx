b95ac611a125f6bad30d9337fe220e59cd9d6665
leshen <lexuar2000@gmail.com>
2022-10-05 16:38:41 +0300

    Body replaced by children, added animations

--- app/components/ui/accordion/Faq.tsx
+++ app/components/ui/accordion/Faq.tsx
@@ -1,34 +1,66 @@
-import { FC, useCallback, useState } from 'react';
-import { Transition, config, animated, useSpring, Spring } from 'react-spring';
+import { FC, useState } from 'react';
+import { animated, config, Spring } from 'react-spring';
 import classNames from './Faq.module.scss';
 
 interface IProps {
 	header: string;
-	body: string;
+	children: React.ReactNode
 }
 
-const Faq: FC<IProps> = ({ header, body }) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
+const Faq: FC<IProps> = ({ header, children }) => {
+	const [isClosed, setIsClosed] = useState<boolean>(true);
+
 	const expand = () => {
-		setIsOpen((prev) => !prev);
+		setIsClosed((prev) => !prev);
 	};
 
 	return (
 		<div className={classNames.block}>
+			<Spring
+				from={{ transform: 'rotate(45deg)', color: '#4080F5' }}
+				to={{ transform: 'rotate(0deg)', color: '#1d1d1d' }}
+				reverse={isClosed}
+				config={config.stiff}
+			>
+				{(styles) => (
+					// <animated.div style={styles} className={classNames.closeBtn}>
+						<animated.svg
+							className={classNames.closeBtn}
+							stroke="currentColor"
+							fill="currentColor"
+							strokeWidth="0"
+							viewBox="0 0 24 24"
+							style={styles}
+							height="22"
+							width="22"
+							xmlns="http://www.w3.org/2000/svg"
+							onClick={expand}
+						>
+							<path
+								fill="none"
+								// stroke="#000"
+								strokeWidth="2"
+								d="M3,3 L21,21 M3,21 L21,3"
+							></path>
+						</animated.svg>
+					// </animated.div>
+				)}
+			</Spring>
+
 			<div className={classNames.header} onClick={expand}>
 				{header}
 			</div>
 
 			<Spring
 				// items={isOpen}
-				from={{ maxHeight: '0px' }}
-				to={{ maxHeight: '100px' }}
-				reverse={isOpen}
-				config={config.stiff}
+				from={{ maxHeight: '0px', marginTop: '0em' }}
+				to={{ maxHeight: '100px', marginTop: '1em' }}
+				reverse={isClosed}
+				// config={config.stiff}
 			>
 				{(styles) => (
 					<animated.div style={styles} className={classNames.body}>
-						{body}
+						{children}
 					</animated.div>
 				)}
 			</Spring>
