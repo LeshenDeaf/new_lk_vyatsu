ba9dd9ce94a6c1a6d09b77c06ffbfef34c8fabaf
leshen <lexuar2000@gmail.com>
2022-10-06 14:52:25 +0300

    Links are styled now

--- app/components/ui/faq/Faq.tsx
+++ app/components/ui/faq/Faq.tsx
@@ -1,10 +1,33 @@
-import { FC, memo } from 'react';
+import Link from 'next/link';
+import { FC, memo, useCallback } from 'react';
 import { IFaq } from '../../../models/api/IFaq';
 import { useFaqQuery } from '../../../services/faq/FaqService';
 import FaqElement from './FaqElement';
+import classNames from './Faq.module.scss';
+import reactStringReplace from 'react-string-replace';
 
 const Faq: FC = () => {
 	const { isLoading, isError, data } = useFaqQuery();
+
+	const makeLinks = useCallback((paragraph: string) => {
+		const regex = /<a rel="noreferrer" href="(.*)">(.*)<\/a>/gm;
+
+		return reactStringReplace(
+			paragraph
+				.replaceAll('&lt;', '<')
+				.replaceAll('&gt;', '>')
+				.replaceAll('&quot;', '"'),
+			regex,
+			(match) =>
+				match ? (
+					<><Link href={`https://new.vyatsu.ru${match}`} target="_blank" passHref>
+						<a target="_blank" className={classNames.link}>{`https://new.vyatsu.ru${match}`}</a>
+					</Link>{' '}</>
+				) : (
+					''
+				)
+		);
+	}, []);
 
 	return (
 		<>
@@ -13,7 +36,7 @@ const Faq: FC = () => {
 				data.map((el: IFaq) => (
 					<FaqElement key={el.id} header={el.name}>
 						{el.answer.map((ans, i) => (
-							<div key={`${el.id}-${i}`}>{ans}</div>
+							<div key={`${el.id}-${i}`}>{ makeLinks(ans) }</div>
 						))}
 					</FaqElement>
 				))}
