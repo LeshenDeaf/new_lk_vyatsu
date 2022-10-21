61d83c6268846d68fcb439d6a21b7e6750753172
leshen <lexuar2000@gmail.com>
2022-10-21 11:48:24 +0300

    Updated user model according to backend

--- app/models/IUser.ts
+++ app/models/IUser.ts
@@ -3,6 +3,12 @@ export interface IUser {
   login: string;
   groups: number[];
   fio: IFio;
+  rights: IRights;
+  logged_as: ILoggedAs;
+}
+
+export interface ILoggedAs extends IUser {
+  info: IEmployeeInfo | IStudentInfo;
 }
 
 export interface IFio {
@@ -10,4 +16,28 @@ export interface IFio {
   last_name: string;
   second_name?: string;
   full?: string;
+}
+
+export interface IRights {
+  is_student: boolean;
+  is_employee: boolean;
+  is_admin: boolean;
+}
+
+export interface IEmployeeInfo {
+  tabnum: number;
+  fio: string;
+  fio_small: string;
+  birthday: string;
+  pasp: IPasport;
+  snils: string;
+  inn: string;
+  age: number;
+}
+
+export interface IStudentInfo {}
+
+export interface IPasport {
+  ser: string;
+  nubmer: string;
 }
