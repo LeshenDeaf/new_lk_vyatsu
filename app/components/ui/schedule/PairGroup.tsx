faf69c3e044eb3ee120d099ec359f9d05d00b06d
leshen <lexuar2000@gmail.com>
2022-10-21 14:36:31 +0300

    Added group showing for teachers

--- app/components/ui/schedule/PairGroup.tsx
+++ app/components/ui/schedule/PairGroup.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import React, { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useAppSelector } from '../../../hooks/redux';
 import { IPairGroup } from '../../../models/schedule';
+import { selectUser } from '../../../store/reducers/UserSlice';
 import styles from './Schedule.module.scss';
 
 interface Props {
@@ -10,6 +12,12 @@ interface Props {
 }
 
 const PairGroup: FC<Props> = ({ pairGroup, teacherClicked }) => {
+	const {data: user} = useAppSelector(selectUser);
+
+	if (!user) {
+		return <></>;
+	}
+
 	return (
 		<div className={styles.pairGroup}>
 			<div className={styles.time}>
@@ -18,12 +26,16 @@ const PairGroup: FC<Props> = ({ pairGroup, teacherClicked }) => {
 			{pairGroup.pairs.map((pair) => (
 				<div className={styles.pair} key={uuidv4()}>
 					{pair.subject_name}, {pair.norm_comment},{' '}
-					<u
-						className="cursor-pointer"
-						onClick={() => teacherClicked(pair.sotr_tabnum, pair.sotr_fio)}
-					>
-						{pair.sotr_fio}
-					</u>{' '}
+					{
+						user.logged_as.rights.is_student 
+							? <u
+								className="cursor-pointer"
+								onClick={() => teacherClicked(pair.sotr_tabnum, pair.sotr_fio)}
+							>
+								{pair.sotr_fio}
+							</u>
+							: <>{pair.group_name}</>
+					}{', '}
 					{pair.mesto.substring(0, 4) === 'http' ? (
 						<Link href={pair.mesto}>
 							<a className="underline" target="_blank">Ссылка на занятие</a>
