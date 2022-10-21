b65d00f3f62e3385df7469221d63941773bed8a8
leshen <lexuar2000@gmail.com>
2022-10-21 11:46:57 +0300

    Added login as api

---
+++ pages/api/auth/login_as.ts
@@ -0,0 +1,43 @@
+import { AxiosResponse } from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { setCookieTokens } from '../../../app/helpers/api/set-cookie-tokens';
+import {
+	AuthAPIResponse,
+	AuthResponse,
+} from '../../../app/models/api/auth/types';
+import vyatsuApi from '../../../app/services/VyatsuApi';
+
+export default async function handle(
+	req: NextApiRequest,
+	res: NextApiResponse<AuthResponse>
+) {
+	return new Promise<void>((resolve) => {
+		const { login } = req.body;
+
+		vyatsuApi
+			.post(
+				'/api_mobile/v2/auth/login_as/',
+				{ login },
+				{
+					headers: { Authorization: req.headers.authorization || '' },
+				}
+			)
+			.then((result: AxiosResponse<AuthAPIResponse>) => {
+				setCookieTokens(
+					res,
+					result.data.access_token,
+					result.data.refresh_token
+				);
+
+				res.status(200).json({
+					token: result.data.access_token,
+					isAuth: true,
+				});
+				return resolve();
+			})
+			.catch((error) => {
+				res.status(error.response.status).json(error.response.data);
+				return resolve();
+			});
+	});
+}
