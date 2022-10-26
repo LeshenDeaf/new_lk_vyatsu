92dc1ded5dfed1c40343c7582646ed5f4679b8c9
leshen <lexuar2000@gmail.com>
2022-10-26 12:02:02 +0300

    bugfix

--- pages/api/edu/schedule/personal.ts
+++ pages/api/edu/schedule/personal.ts
@@ -7,19 +7,17 @@ export default async function handle(
 	res: NextApiResponse<any>
 ) {
 	return new Promise<void>((resolve) => {
-		if (req.method === 'GET') {
-			vyatsuApi
-				.get('/api_mobile/v2/edu/schedule/personal/', {
-					headers: { Authorization: req.headers.authorization || '' },
-				})
-				.then((r: AxiosResponse<any>) => {
-					res.status(200).json(r.data);
-					return resolve();
-				})
-				.catch((e) => {
-					res.status(e.response.status).json(e.response.data);
-					return resolve();
-				});
-		}
+		vyatsuApi
+			.get('/api_mobile/v2/edu/schedule/personal/', {
+				headers: { Authorization: req.headers.authorization || '' },
+			})
+			.then((r: AxiosResponse<any>) => {
+				res.status(200).json(r.data);
+				return resolve();
+			})
+			.catch((e) => {
+				res.status(e.response.status).json(e.response.data);
+				return resolve();
+			});
 	});
 }
