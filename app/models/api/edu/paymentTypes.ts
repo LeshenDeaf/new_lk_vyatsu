0680930ee2e413e7fad3ba8e42fec3d68e4fc25a
leshen <lexuar2000@gmail.com>
2022-10-31 10:56:30 +0300

    Types for payments

---
+++ app/models/api/edu/paymentTypes.ts
@@ -0,0 +1,29 @@
+export interface PaymentType {
+  payment_type: string;
+  dept: number;
+}
+
+export interface PaymentGraph {
+  graf_id: number;
+  fis_id_id: number;
+  uid_fis_face: string;
+  student_id: number;
+  UID_student_id: string;
+  num_dog: string;
+  date_dog: string;
+  UID_dog: string;
+  grafic_pay: Payment[];
+  fact_pay: RealPayment[];
+}
+
+export interface Payment {
+  DataPay: string;
+  Summa: number;
+}
+
+export interface RealPayment {
+  DataPaySt: string;
+  SummaSt: number;
+  Return: boolean;
+}
+
