3c36d8521f547e72528542cd2ad60b964bd23624
leshen <lexuar2000@gmail.com>
2022-10-31 14:31:27 +0300

    Added questions render

--- pages/edu/payments.tsx
+++ pages/edu/payments.tsx
@@ -3,6 +3,7 @@ import { NextPage } from 'next';
 import { useState } from 'react';
 import {
 	useGraphQuery,
+	usePaymentQuestionsQuery,
 	useTypesListQuery,
 } from '../../app/services/edu/PaymentsService';
 import { setTitle } from '../../app/store/reducers/TitleSlice';
@@ -22,6 +23,13 @@ const Payments: NextPage = () => {
 		isError,
 		error,
 	} = useTypesListQuery();
+	const {
+		data: questions,
+		isLoading: isLoadingQ,
+		isFetching: isFetchingQ,
+		isError: isErrorQ,
+		error: errorQ,
+	} = usePaymentQuestionsQuery();
 
 	const {
 		data: graph,
@@ -48,39 +56,65 @@ const Payments: NextPage = () => {
 
 	if (isError && error) {
 		const err = error as FetchBaseQueryError;
-		err.data = err.data as { message: string };
 		return <>{err.data?.message}</>;
 	}
 
 	return (
-		<div className="w-full">
-			{types?.map((type) => (
-				<Accordion
-					key={type.payment_type}
-					header={`${type.payment_type}. Долг: ${type.dept}`}
-					onClick={loadGraph(type.payment_type)}
-				>
-					{graph?.map((el) => (
-						<div key={el.graf_id} className="flex flex-column">
-							<div className="mr-4">
-								{el.grafic_pay.map((gpay) => (
-									<div key={`${el.graf_id}-${gpay.DataPay}-${gpay.Summa}`}>
-										{dayjs(gpay.DataPay).format(dateFormat)} - {gpay.Summa} ₽
-									</div>
-								))}
+		<>
+			<div className="w-full">
+				{types?.map((type) => (
+					<Accordion
+						key={type.payment_type}
+						header={`${type.payment_type}. Долг: ${type.dept}`}
+						onClick={loadGraph(type.payment_type)}
+					>
+						{isLoadingG || isFetchingG ? 'Загрузка...' : ''}
+						{graph?.map((el) => (
+							<div key={el.graf_id} className="flex flex-column">
+								<div className="mr-4">
+									{el.grafic_pay.map((gpay) => (
+										<div key={`${el.graf_id}-${gpay.DataPay}-${gpay.Summa}`}>
+											{dayjs(gpay.DataPay).format(dateFormat)} - {gpay.Summa} ₽
+										</div>
+									))}
+								</div>
+								<div>
+									{el.fact_pay.map((fpay) => (
+										<div
+											key={`${el.graf_id}-${fpay.DataPaySt}-${fpay.SummaSt}`}
+										>
+											{dayjs(fpay.DataPaySt).format(dateFormat)} -{' '}
+											{fpay.SummaSt} ₽
+										</div>
+									))}
+								</div>
 							</div>
-							<div>
-								{el.fact_pay.map((fpay) => (
-									<div key={`${el.graf_id}-${fpay.DataPaySt}-${fpay.SummaSt}`}>
-										{dayjs(fpay.DataPaySt).format(dateFormat)} - {fpay.SummaSt} ₽
-									</div>
-								))}
-							</div>
+						))}
+					</Accordion>
+				))}
+			</div>
+			<div>
+				{questions?.map((q) => (
+					<div
+						className="my-6 relative p-6 pl-6 shadow-dark rounded-[10px] bg-white"
+						key={q.id}
+					>
+						<div className="pl-5">
+							<span className="absolute text-blue-600 top-4 left-6 text-3xl">
+								?
+							</span>
+							{q.text}
 						</div>
-					))}
-				</Accordion>
-			))}
-		</div>
+						<div className="text-slate-800 mt-2 pl-5">
+							<span className="absolute text-blue-600 top-12 left-6 text-2xl">
+								✓
+							</span>
+							{q.answer}
+						</div>
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
