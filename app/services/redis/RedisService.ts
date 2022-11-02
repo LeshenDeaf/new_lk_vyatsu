aee8c25a7dd6f50395b110ece19b858d14e24306
leshen <lexuar2000@gmail.com>
2022-11-02 14:19:39 +0300

    Moved functionality to another class

--- app/services/redis/RedisService.ts
+++ app/services/redis/RedisService.ts
@@ -1,9 +1,13 @@
 import { client, connect } from '../../../lib/redis';
-import RedisService from './RedisService';
-/* 
-class RedisUserService {
-	expiresIn = 3600;
-	prefix = 'User';
+
+class RedisService {
+	expiresIn;
+	prefix;
+
+	constructor(prefix: string, expiresIn: number = 3600) {
+		this.prefix = prefix;
+		this.expiresIn = expiresIn;
+	}
 
 	async create(data: any) {
 		await connect();
@@ -25,20 +29,16 @@ class RedisUserService {
 		return data.id;
 	}
 
-	async find(userId: number) {
+	async find(elementId: number) {
 		await connect();
 
 		const res = (await client.execute([
 			'JSON.GET',
-			`${this.prefix}:${userId}`,
+			`${this.prefix}:${elementId}`,
 		])) as string;
 
 		return JSON.parse(res);
 	}
 }
- */
-// const redisUserService = new RedisUserService();
 
-const redisUserService = new RedisService('User', 3600);
-
-export default redisUserService;
+export default RedisService;
