0734325e62d5a2cf367120f81958d3f1b544e1b1
leshen <lexuar2000@gmail.com>
2022-11-02 14:23:21 +0300

    Removed comments

--- app/services/redis/RedisUserService.ts
+++ app/services/redis/RedisUserService.ts
@@ -1,43 +1,4 @@
-import { client, connect } from '../../../lib/redis';
 import RedisService from './RedisService';
-/* 
-class RedisUserService {
-	expiresIn = 3600;
-	prefix = 'User';
-
-	async create(data: any) {
-		await connect();
-
-		data.id = parseInt(data.id);
-
-		await client.execute([
-			'JSON.SET',
-			`${this.prefix}:${data.id}`,
-			'$',
-			JSON.stringify(data),
-		]);
-		await client.execute([
-			'EXPIRE',
-			`${this.prefix}:${data.id}`,
-			this.expiresIn,
-		]);
-
-		return data.id;
-	}
-
-	async find(userId: number) {
-		await connect();
-
-		const res = (await client.execute([
-			'JSON.GET',
-			`${this.prefix}:${userId}`,
-		])) as string;
-
-		return JSON.parse(res);
-	}
-}
- */
-// const redisUserService = new RedisUserService();
 
 const redisUserService = new RedisService('User', 3600);
 
