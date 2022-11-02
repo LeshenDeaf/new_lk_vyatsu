7684bbc356f59419ca98da9b61404bfbf9ee5a97
leshen <lexuar2000@gmail.com>
2022-11-02 11:57:26 +0300

    removed redundant isNull

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,8 +4,6 @@ export const client = new Client();
 
 export async function connect() {
 	if (!client.isOpen()) {
-		await client.open(
-			process.env.REDIS_URL ?? 'redis://default:pswrd@127.0.0.1:6379'
-		);
+		await client.open(process.env.REDIS_URL);
 	}
 }
