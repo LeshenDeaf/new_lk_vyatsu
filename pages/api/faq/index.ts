1ac3cc9993c5cb3363f6156430914eb5d52ee31e
leshen <lexuar2000@gmail.com>
2022-11-03 13:13:28 +0300

    Remade api to use redis cache

--- pages/api/faq/index.ts
+++ pages/api/faq/index.ts
@@ -3,28 +3,40 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { AxiosResponse } from 'axios';
 import vyatsuApi from '../../../app/services/VyatsuApi';
 import { IFaq } from '../../../app/models/api/IFaq';
+import redisFaqService from '../../../app/services/redis/RedisFaqService';
+
+const getFaqInfo = async (url: string, token: string) => {
+	const faq = await redisFaqService.find(url);
+
+	if (faq && Object.keys(faq).length !== 0 && faq.length !== 0) {
+		console.log('Faq read from redis');
+		return faq;
+	}
+	console.log('Faq read from API');
+
+	const r = (await vyatsuApi.post(
+		'/api_mobile/v2/faq/get/',
+		{ url },
+		{ headers: { Authorization: token || '' } }
+	)) as AxiosResponse<IFaq[]>;
+	
+	redisFaqService.create(r.data);
+
+	return r.data;
+};
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<IFaq[]>
 ) {
 	return new Promise<void>((resolve) => {
-		vyatsuApi
-			.post(
-				'/api_mobile/v2/faq/get/',
-				{
-					url: req.body.url,
-				},
-				{
-					headers: { Authorization: req.headers.authorization || '' },
-				}
-			)
-			.then((r: AxiosResponse<IFaq[]>) => {
-				res.status(200).json(r.data);
+		getFaqInfo(req.body.url, req.headers.authorization || '')
+			.then((faq) => {
+				res.status(200).json(faq);
 				return resolve();
 			})
-			.catch((e) => {
-				res.status(e.response?.status).json(e.response?.data);
+			.catch((error) => {
+				res.status(error.response?.status || 400).json(error.response?.data || error.message);
 				return resolve();
 			});
 	});
