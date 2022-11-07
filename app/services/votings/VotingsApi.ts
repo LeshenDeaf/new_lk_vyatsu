80bf277cfa42725a3ffd4bd5f1792037c1911b8b
leshen <lexuar2000@gmail.com>
2022-11-07 15:55:40 +0300

    Injected new endpoint to vote

--- app/services/votings/VotingsApi.ts
+++ app/services/votings/VotingsApi.ts
@@ -1,25 +1,38 @@
 import { IVoting } from '../../models/api/votings/types';
 import { basicVyatsu } from '../BasicVyatsu';
-import { IQuestion } from './../../models/api/votings/types';
+import { IQuestion, IVoteRequest } from './../../models/api/votings/types';
 
 export const votingsApi = basicVyatsu.injectEndpoints({
 	endpoints: (builder) => ({
 		votings: builder.query<IVoting[], void>({
 			query: () => 'api/votings/list',
-			providesTags: ['Votings']
+			providesTags: ['Votings'],
 		}),
 		voting: builder.query<IVoting, number>({
 			query: (votingId) => `api/votings/${votingId}`,
-			providesTags: ['Votings']
+			providesTags: ['Votings'],
 		}),
 		questions: builder.query<IQuestion[], number>({
 			query: (vote_id: number) => ({
 				url: `api/votings/questions`,
 				method: 'POST',
-				body: {vote_id}
+				body: { vote_id },
 			}),
-		})
+		}),
+		vote: builder.mutation<void, IVoteRequest>({
+			query: (req) => ({
+				url: 'api/votings/vote',
+				method: 'POST',
+				body: req,
+			}),
+			invalidatesTags: ['Votings'],
+		}),
 	}),
 });
 
-export const { useVotingsQuery, useVotingQuery, useQuestionsQuery } = votingsApi;
+export const {
+	useVotingsQuery,
+	useVotingQuery,
+	useQuestionsQuery,
+	useVoteMutation,
+} = votingsApi;
