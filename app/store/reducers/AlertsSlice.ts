e156d6571fcf5a9c2ce2421e27c8e4c8f2740ec8
leshen <lexuar2000@gmail.com>
2022-12-19 16:16:50 +0300

    Made reducer for alerts

---
+++ app/store/reducers/AlertsSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { v4 } from 'uuid';
+
+export enum AlertTypes {
+	Success,
+	Info,
+	Warning,
+	Danger,
+}
+
+export type AlertInfo = {
+  id?: string;
+  title: string;
+  text: string;
+  alertType: AlertTypes
+  time?: number;
+}
+
+export type AlertsState = Array<any>;
+
+const initialState: AlertsState = [];
+
+export const alertsSlice = createSlice({
+	name: 'alerts',
+	initialState,
+	reducers: {
+		add: (
+			state: AlertsState,
+			action: PayloadAction<AlertInfo>
+		) => {
+      action.payload.id ??= v4();
+      state.push(action.payload);
+      return state;
+		},
+    remove: (
+      state: AlertsState,
+      action: PayloadAction<string>
+    ) => {
+      state.filter(value => value.id === action.payload);
+    }
+	}
+});
+
+export const { add, remove } = alertsSlice.actions;
+
+export const selectAlerts = (state: RootState) => state.alerts;
