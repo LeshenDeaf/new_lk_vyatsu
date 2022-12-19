6a86dab1b905ae43fc54d54f4de39496698c5e7d
leshen <lexuar2000@gmail.com>
2022-12-19 16:14:14 +0300

    Added invalidateAll function

--- app/components/screens/menus/header/LoginAs.tsx
+++ app/components/screens/menus/header/LoginAs.tsx
@@ -7,23 +7,37 @@ import en from '../../../../../lang/en/header.json';
 import ru from '../../../../../lang/ru/header.json';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import { useEnter } from '../../../../hooks/useEnter';
-import { useInvalidate } from '../../../../hooks/useInvalidate';
+import {
+	reducerInfo,
+	useInvalidate,
+	useInvalidateCallback,
+} from '../../../../hooks/useInvalidate';
 import { useLoginAsMutation } from '../../../../services/auth/AuthService';
-import { programsApi } from '../../../../services/edu/ProgramsService';
-import { scheduleApi } from '../../../../services/edu/ScheduleService';
-import { votingsApi } from '../../../../services/votings/VotingsApi';
+import { paymentsInvalidation } from '../../../../services/edu/PaymentsService';
+import { programsInvalidation } from '../../../../services/edu/ProgramsService';
+import { scheduleInvalidation } from '../../../../services/edu/ScheduleService';
+import { votingsInvalidation } from '../../../../services/votings/VotingsApi';
 import { setAuthData } from '../../../../store/reducers/AuthSlice';
 import { selectUser, setUserData } from '../../../../store/reducers/UserSlice';
-import { paymentsApi } from '../../../../services/edu/PaymentsService';
+
+const mustBeInvalidated: reducerInfo[] = [
+	programsInvalidation,
+	scheduleInvalidation,
+	votingsInvalidation,
+	paymentsInvalidation,
+];
+
+const invalidateAll = (invalidate: useInvalidateCallback) =>
+	mustBeInvalidated.forEach(invalidate);
 
 const LoginAs: FC = () => {
 	const { locale } = useRouter();
-	const lang = useMemo(() => locale === 'en' ? en : ru, [locale]);
+	const lang = useMemo(() => (locale === 'en' ? en : ru), [locale]);
 
 	const user = useAppSelector(selectUser);
 
 	const [login, setLogin] = useState<string>(user.data?.logged_as.login ?? '');
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 	const invalidate = useInvalidate();
 
 	const [loginAs, loginAsRes] = useLoginAsMutation();
@@ -40,12 +54,7 @@ const LoginAs: FC = () => {
 				});
 
 				dispatch(setUserData(userData.data));
-
-				invalidate(scheduleApi.reducerPath, ['Schedule']);
-				invalidate(programsApi.reducerPath, ['Programs']);
-				invalidate(votingsApi.reducerPath, ['Votings']);
-				invalidate(paymentsApi.reducerPath, ['Payments']);
-				// await router.push('/');
+				invalidateAll(invalidate);
 			} catch (e) {
 				console.error(e);
 			}
@@ -57,7 +66,7 @@ const LoginAs: FC = () => {
 		}
 	}, [dispatch, invalidate, login, loginAs]);
 
-	const handleKeyDown = useEnter(submit); 
+	const handleKeyDown = useEnter(submit);
 
 	return (
 		<div className="px-4 sm:px-10 flex flex-col justify-center w-full sm:w-1/4 md:w-1/4 xl:w-1/3 2xl:w-1/4">
@@ -65,13 +74,14 @@ const LoginAs: FC = () => {
 				<input
 					className="w-full border-0 outline-0 leading-10"
 					placeholder={lang.loginAs}
-          onChange={(e) => setLogin(e.target.value ?? '')}
+					onChange={(e) => setLogin(e.target.value ?? '')}
 					onKeyDown={handleKeyDown}
-          value={login}
+					value={login}
 				/>
-				<div className="cursor-pointer flex items-center justify-center w-6"
-          onClick={submit}
-        >
+				<div
+					className="cursor-pointer flex items-center justify-center w-6"
+					onClick={submit}
+				>
 					<Image
 						src="/images/arrow_down.svg"
 						alt="search"
