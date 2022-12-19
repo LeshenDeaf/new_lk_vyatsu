525b28a866a614b8be1105d369bd764f802414f2
leshen <lexuar2000@gmail.com>
2022-12-19 17:12:43 +0300

    Layout fix

--- app/components/ui/alerts/Alerts.tsx
+++ app/components/ui/alerts/Alerts.tsx
@@ -3,13 +3,17 @@ import { selectAlerts } from '../../../store/reducers/AlertsSlice';
 import { Alert } from './Alert';
 
 export const Alerts = () => {
-  const alerts = useAppSelector(selectAlerts);
+	const alerts = useAppSelector(selectAlerts);
 
-  return (
-    <div id="alerts" className="fixed flex flex-col right-0 top-10">
-      {alerts.map(alert => {
-        return <Alert key={alert.id} {...alert} />
-      })}
-    </div>
-  )
-}
+	return (
+		<div
+			id="alerts"
+			className="fixed flex flex-col-reverse right-0 top-10"
+			style={{transition: ".2s all ease-in-out"}}
+		>
+			{alerts.map((alert) => {
+				return <Alert key={alert.id} {...alert} />;
+			})}
+		</div>
+	);
+};
