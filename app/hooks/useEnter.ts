505ec42d3e45cc63f18d0d7275e7a515b38af3c6
leshen <lexuar2000@gmail.com>
2022-12-19 16:14:46 +0300

    refactoring

--- app/hooks/useEnter.ts
+++ app/hooks/useEnter.ts
@@ -1,9 +1,9 @@
 import {KeyboardEvent} from 'react';
 
-export const useEnter = (clbck: () => void) => {
+export const useEnter = (callback: () => void) => {
   return (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      clbck()
+      callback()
     }
   }
 }
