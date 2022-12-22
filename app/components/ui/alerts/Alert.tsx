889d65d9c4e4d93b4a307c71584f66bc6dad1e90
leshen <lexuar2000@gmail.com>
2022-12-22 10:52:11 +0300

    Change ui updated method names

--- app/components/ui/alerts/Alert.tsx
+++ app/components/ui/alerts/Alert.tsx
@@ -1,22 +1,15 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
-import { Portal } from '../portals/Portal';
+import { Transition, animated, easings, useSpring } from '@react-spring/web';
+import { FC, useEffect, useRef, useState } from 'react';
 import {
-	MdOutlineInfo,
+	MdCheckCircleOutline,
 	MdClose,
 	MdOutlineDangerous,
+	MdOutlineInfo,
 	MdOutlineWarningAmber,
-	MdCheckCircleOutline,
 } from 'react-icons/md';
-import {
-	Transition,
-	Spring,
-	animated,
-	config,
-	easings,
-	useSpring,
-} from '@react-spring/web';
-import { AlertTypes, remove } from '../../../store/reducers/AlertsSlice';
 import { useAppDispatch } from '../../../hooks/redux';
+import { AlertTypes, removeAlert } from '../../../store/reducers/AlertsSlice';
+import { Portal } from '../portals/Portal';
 
 const alertColors = {
 	[AlertTypes.Success]: 'rgb(22 163 74)',
@@ -64,13 +57,13 @@ export const Alert: FC<AlertProps> = ({ id, title, text, alertType, time }) => {
 	const [isShown, setIsShown] = useState<boolean>(true);
 	const dispatch = useAppDispatch();
 	const springProps = useSpring({
-		from: { width: '0%' },
-		to: { width: '100%' },
+		from: { width: '100%' },
+		to: { width: '0%' },
 		config: {
-			duration: time
-		}
+			duration: time,
+		},
 	});
-	const ref = useRef<HTMLDivElement>(null)
+	const ref = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		let timeoutId: ReturnType<typeof setTimeout> | undefined;
@@ -88,25 +81,37 @@ export const Alert: FC<AlertProps> = ({ id, title, text, alertType, time }) => {
 		<Portal container={document && document.getElementById('alerts')}>
 			<Transition
 				items={isShown}
-				from={{ opacity: 0, transform: 'translateX(250px)', marginBottom: `-100px` }}
-				enter={{ opacity: 1, transform: 'translateX(0px)', marginBottom: '20px' }}
-				leave={{ opacity: 0, transform: 'translateX(250px)', marginBottom: `-${ref?.current?.clientHeight || 0}px` }}
+				from={{
+					opacity: 0,
+					transform: 'translateX(250px)',
+					marginBottom: `-100px`,
+				}}
+				enter={{
+					opacity: 1,
+					transform: 'translateX(0px)',
+					marginBottom: '20px',
+				}}
+				leave={{
+					opacity: 0,
+					transform: 'translateX(250px)',
+					marginBottom: `-${ref?.current?.clientHeight || 0}px`,
+				}}
 				reverse={isShown}
-				config={{easing: easings.easeInOutCubic, tension: 400, friction: 26}}
-				onDestroyed={() => dispatch(remove(id))}
+				config={{ easing: easings.easeInOutCubic, tension: 400, friction: 26 }}
+				onDestroyed={() => dispatch(removeAlert(id))}
 			>
 				{(styles, item) =>
 					item && (
 						<animated.div
 							style={styles}
-							className="right-10 cursor-pointer relative z-[999] bg-white max-w-md p-4 pl-6 rounded-md shadow-dark overflow-hidden"
-							onClick={() => setIsShown(false)}
+							className="right-10 relative z-[999] bg-white max-w-md p-4 pl-6 rounded-md shadow-dark overflow-hidden"
+							// onClick={() => setIsShown(false)}
 							ref={ref}
 						>
-							<div
+							{!time && <div
 								className={`absolute top-0 left-0 h-full w-1`}
 								style={{ backgroundColor: alertColors[alertType] }}
-							></div>
+							></div>}
 							<div className="flex">
 								<div className="h-full w-6 mr-4">{alertIcons[alertType]}</div>
 								<div className="text break-words">
@@ -122,8 +127,8 @@ export const Alert: FC<AlertProps> = ({ id, title, text, alertType, time }) => {
 							</div>
 							{time && (
 								<animated.div
-									className="bg-red-500 h-5"
-									style={{ width: springProps.width.to((w) => `${w}`) }}
+									className="h-1 absolute bottom-0 left-0 right-0"
+									style={{ width: springProps.width.to((w) => `${w}`), backgroundColor: alertColors[alertType] }}
 								></animated.div>
 							)}
 						</animated.div>
