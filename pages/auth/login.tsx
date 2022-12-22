24316a6c8f999c7da724ba92493c71f3a3a5d4cb
leshen <lexuar2000@gmail.com>
2022-12-22 15:43:00 +0300

    Added alerts for login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -5,13 +5,14 @@ import { useRouter } from 'next/router';
 import { Suspense, useCallback } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ILoginFormFields } from '../../app/components/screens/auth/form/types';
-import { useAppDispatch } from '../../app/hooks/redux';
+import { useAlert } from '../../app/hooks/useAlert';
 import { LoginRequest } from '../../app/models/api/auth/types';
 import { useLoginMutation } from '../../app/services/auth/AuthService';
 import { setAuthData } from '../../app/store/reducers/AuthSlice';
 import { setTitle } from '../../app/store/reducers/TitleSlice';
 import { setUserData } from '../../app/store/reducers/UserSlice';
 import { wrapper } from '../../app/store/store';
+import { useAppDispatch } from '../../app/hooks/redux';
 
 const DynamicLoginForm = dynamic(
 	() => import('../../app/components/screens/auth/form/LoginForm'),
@@ -23,14 +24,15 @@ const DynamicLoginForm = dynamic(
 
 const Login: NextPage = () => {
 	const [login, loginRes] = useLoginMutation();
-	const dispatch = useAppDispatch();
 	const router = useRouter();
+	const dispatch = useAppDispatch();
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 		setError,
 	} = useForm<ILoginFormFields>();
+	const alerts = useAlert();
 
 	const onSubmit: SubmitHandler<ILoginFormFields> = useCallback(
 		async (data) => {
@@ -48,22 +50,34 @@ const Login: NextPage = () => {
 					dispatch(setUserData(userData.data));
 
 					await router.push('/');
+					alerts.success({
+						title: 'Вы авторизованы',
+						text: '',
+						time: 2000,
+					});
 				} catch (e) {
 					console.error(e);
+					alerts.danger({
+						title: 'Ошибка получения данных о пользователе',
+						text: 'Во время авторизации произошла ошибка. Перезагрузите страницу.',
+					});
 				}
-			} else if (
-				typeof res === 'string' || res instanceof String ||
-				'error' in res &&
-				'data' in res.error &&
-				'errors' in (res.error.data as any)
-			) {
+			} else if ('status' in res.error && res.error.status === 503) {
+				alerts.danger({
+					title: 'Ошибка авторизации',
+					text: `Сервис авторизации временно недоступен. 
+					Перезагрузите страницу и повторите попытку позже. 
+					Если ошибка не исчезнет, обратитесь к администратору`,
+					time: 10000
+				});
+			} else {
 				setError('login', {
 					type: 'custom',
 					message: 'Неверный логин или пароль',
 				});
 			}
 		},
-		[router, dispatch, login, setError]
+		[login, dispatch, router, alerts, setError]
 	);
 
 	return (
@@ -74,6 +88,18 @@ const Login: NextPage = () => {
 				register={register}
 				isLoading={loginRes.isLoading}
 			/>
+
+			<button
+				onClick={() =>
+					alerts.success({
+						title: 'Title of notification',
+						text: 'And this is a text uaaaAnd this is a text uaaaAnd this is a text uaaaAnd this is a text uaaaAnd this is a text uaaaAnd this is a text uaaa',
+						time: 5000,
+					})
+				}
+			>
+				SHOW|HIDE
+			</button>
 		</Suspense>
 	);
 };
