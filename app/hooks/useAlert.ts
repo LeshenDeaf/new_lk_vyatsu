136febde56bf31b83a263942a5846ebbe820d1eb
leshen <lexuar2000@gmail.com>
2022-12-22 10:46:17 +0300

    Created useAlert hook

--- app/hooks/useAlert.ts
+++ app/hooks/useAlert.ts
@@ -1,3 +1,34 @@
-export function useAlert() {
-  return [];
+import { useAppDispatch } from './redux';
+import {
+	AlertTypes,
+	addSuccessAlert,
+	addInfoAlert,
+	addDangerAlert,
+	addWarningAlert,
+} from '../../app/store/reducers/AlertsSlice';
+
+type AlertArguments = {
+	title: string;
+	text: string;
+	time?: number;
+};
+
+type AlertFunction = (alertInfo: AlertArguments) => void;
+
+interface UseAlertResult {
+	success: AlertFunction;
+	info: AlertFunction;
+	warning: AlertFunction;
+	danger: AlertFunction;
+}
+
+export function useAlert(): UseAlertResult {
+	const dispatch = useAppDispatch();
+
+	return {
+		success: (alertInfo) => dispatch(addSuccessAlert(alertInfo)),
+		info: (alertInfo) => dispatch(addSuccessAlert(alertInfo)),
+		warning: (alertInfo) => dispatch(addSuccessAlert(alertInfo)),
+		danger: (alertInfo) => dispatch(addSuccessAlert(alertInfo)),
+	};
 }
