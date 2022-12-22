e3f88b9c28164bf421eddd5a0cbdb61492fef888
leshen <lexuar2000@gmail.com>
2022-12-22 10:45:59 +0300

    Added new reducers

--- app/store/reducers/AlertsSlice.ts
+++ app/store/reducers/AlertsSlice.ts
@@ -9,15 +9,17 @@ export enum AlertTypes {
 	Danger,
 }
 
-export type AlertInfo = {
-  id?: string;
-  title: string;
-  text: string;
-  alertType: AlertTypes
-  time?: number;
-}
+export type AlertWithoutType = {
+	id?: string;
+	title: string;
+	text: string;
+	time?: number;
+};
 
-export type AlertsState = Array<any>;
+export type AlertInfo = AlertWithoutType & { alertType: AlertTypes };
+
+type AlertState = AlertInfo & {id: string}
+export type AlertsState = Array<AlertState>;
 
 const initialState: AlertsState = [];
 
@@ -25,23 +27,52 @@ export const alertsSlice = createSlice({
 	name: 'alerts',
 	initialState,
 	reducers: {
-		add: (
+		addSuccessAlert: (
 			state: AlertsState,
-			action: PayloadAction<AlertInfo>
+			action: PayloadAction<AlertWithoutType>
 		) => {
-      action.payload.id ??= v4();
-      state.push(action.payload);
-      return state;
+			state.push({ ...action.payload, alertType: AlertTypes.Success, id: v4() });
+			return state;
 		},
-    remove: (
-      state: AlertsState,
-      action: PayloadAction<string>
-    ) => {
-      return state.filter(value => value.id !== action.payload);
-    }
-	}
+		addInfoAlert: (
+			state: AlertsState,
+			action: PayloadAction<AlertWithoutType>
+		) => {
+			state.push({ ...action.payload, alertType: AlertTypes.Info, id: v4() });
+			return state;
+		},
+		addWarningAlert: (
+			state: AlertsState,
+			action: PayloadAction<AlertWithoutType>
+		) => {
+			state.push({ ...action.payload, alertType: AlertTypes.Warning, id: v4() });
+			return state;
+		},
+		addDangerAlert: (
+			state: AlertsState,
+			action: PayloadAction<AlertWithoutType>
+		) => {
+			state.push({ ...action.payload, alertType: AlertTypes.Danger, id: v4() });
+			return state;
+		},
+
+		addAlert: (state: AlertsState, action: PayloadAction<AlertInfo>) => {
+			state.push({...action.payload, id: v4()});
+			return state;
+		},
+		removeAlert: (state: AlertsState, action: PayloadAction<string>) => {
+			return state.filter((value) => value.id !== action.payload);
+		},
+	},
 });
 
-export const { add, remove } = alertsSlice.actions;
+export const {
+	addAlert,
+	addSuccessAlert,
+	addInfoAlert,
+	addWarningAlert,
+	addDangerAlert,
+	removeAlert,
+} = alertsSlice.actions;
 
 export const selectAlerts = (state: RootState) => state.alerts;
